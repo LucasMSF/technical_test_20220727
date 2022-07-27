a84524f97b6186f258c4747faec1cb90e29bdc72
LucasMSF <lucasgg003@gmail.com>
2022-07-27 00:19:27 -0300

    stash compiled js

--- Q1.js
+++
@@ -1,7 +0,0 @@
-// let [i, soma, k]: number[] = [13, 0, 0];
-var i = 13, s = 0, k = 0;
-while (k < i) {
-    k++;
-    s += k;
-}
-console.log(i, s, k);
--- Q5.js
+++
@@ -1,9 +0,0 @@
-var input = 'subi no ônibus';
-function reversetString(input) {
-    var reverseString = '';
-    for (var i = 0; i < input.length; i++) {
-        reverseString += input[input.length - (i + 1)];
-    }
-    return reverseString;
-}
-console.log(reversetString(input));
